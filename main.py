c1a895d2ffd22257029cdecd9ba5094ecbfc0a3b
Erik Madson <erik.madson@gmail.com>
2025-07-27 04:49:26 -0500

    Added a verbose option

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     if len(sys.argv) < 2:
-        print("Usage: uv run main.py <prompt string>")
+        print("Usage: uv run main.py <prompt string> [--verbose]")
+        return
 
     user_prompt = sys.argv[1]
+
+    verbose = False
+    if len(sys.argv) >= 2:
+        if sys.argv[2] == "--verbose":
+            verbose = True
+        else:
+            print("Usage: uv run main.py <prompt string> [--verbose]")
+            return
+
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -26,8 +36,11 @@ def main():
     )
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
